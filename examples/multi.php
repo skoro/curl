7134ef81ff3b7364f559768472dd1b8bf020771c
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2015-11-23 22:23:46 +0200

    Usage Multi curl example.

---
+++ examples/multi.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../Curl.php';
+require __DIR__ . '/../Multi.php';
+
+use skoro\curl\Multi;
+use skoro\curl\Curl;
+
+$multi = new Multi();
+$multi->add(new Curl('http://ru.wikipedia.org'))
+      ->add(new Curl('http://uk.wikipedia.org'))
+      ->add(new Curl('http://google.com'))
+      ->run();
+      
+foreach ($multi as $curl) {
+    print $curl->getUrl() . ' --- ' . $curl->getStatusCode() . PHP_EOL;
+}
+
